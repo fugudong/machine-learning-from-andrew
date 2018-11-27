8b4ccae7e31776162757b9b2a3c4cb91469584bb
embed <1579692451@qq.com>
2018-11-27 14:49:59 +0800

    add ex1

---
+++ machine-learning-ex1/ex1/featureNormalize.m
@@ -0,0 +1,45 @@
+function [X_norm, mu, sigma] = featureNormalize(X)
+%FEATURENORMALIZE Normalizes the features in X 
+%   FEATURENORMALIZE(X) returns a normalized version of X where
+%   the mean value of each feature is 0 and the standard deviation
+%   is 1. This is often a good preprocessing step to do when
+%   working with learning algorithms.
+
+% You need to set these values correctly
+X_norm = X;
+mu = zeros(1, size(X, 2));
+sigma = zeros(1, size(X, 2));
+
+for featureColumn = 1: size(X, 2)
+	mu(1, featureColumn) = mean(X(:, featureColumn));
+	X(:, featureColumn) = X(:, featureColumn).-mu(1, featureColumn);
+	sigma(1, featureColumn) = std(X(:, featureColumn));
+	X(:, featureColumn) = X(:, featureColumn)./sigma(1, featureColumn)
+end;
+% ====================== YOUR CODE HERE ======================
+% Instructions: First, for each feature dimension, compute the mean
+%               of the feature and subtract it from the dataset,
+%               storing the mean value in mu. Next, compute the 
+%               standard deviation of each feature and divide
+%               each feature by it's standard deviation, storing
+%               the standard deviation in sigma. 
+%
+%               Note that X is a matrix where each column is a 
+%               feature and each row is an example. You need 
+%               to perform the normalization separately for 
+%               each feature. 
+%
+% Hint: You might find the 'mean' and 'std' functions useful.
+%       
+
+X_norm = X;
+
+
+
+
+
+
+
+% ============================================================
+
+end
